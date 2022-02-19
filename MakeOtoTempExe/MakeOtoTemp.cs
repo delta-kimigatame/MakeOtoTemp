7bb333b20f669432f09505d6e2ac5e834bfe1dec
delta_kimigatame <sankaku.kuro@gmail.com>
2022-02-19 13:38:38 +0900

    python実行用のexeを作成

---
+++ MakeOtoTempExe/MakeOtoTemp.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+class MakeOtoTemp
+{
+	static void Main(string[] args)
+	{
+		String python = ".\\python-3.9.10\\python.exe";
+		String endpoint = ".\\src\\MakeOtoTemp.py";
+		Process p = Process.Start(python, endpoint +" " + String.Join(" ", args));
+		p.WaitForExit();
+	}
+}
